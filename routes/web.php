151511e549fa9d703b0d94730043c0ba295fe993
Mamed766 <mehemmeddemirli57@gmail.com>
2025-06-14 16:10:03 +0400

    first

---
+++ routes/web.php
@@ -0,0 +1,45 @@
+<?php
+
+use App\Models\Listing;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ListingController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//all listings
+Route::get('/', [ListingController::class, 'index']);
+
+
+
+//Show create form
+Route::get('/listings/create', [ListingController::class, 'create']);
+
+// Store listing data
+Route::post('/listings', [ListingController::class, 'store']);
+
+
+//Show Edit Form
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
+
+
+// Update Listing
+Route::put('/listings/{listing}', [ListingController::class, 'update']);
+
+// Delete Listing
+Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
+
+// single listing
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
+
+//Show Register/Create Form
+Route::get('/register', [UserController::class, 'create']);
